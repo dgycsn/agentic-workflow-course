ed7b9d42a482819702d53213d155a61c54842eff
dgycsn <yalp@student.ethz.ch>
2025-11-30 06:03:07 +0100

    extend prompt

--- module 1/1 prompt exercise.py
+++ module 1/1 prompt exercise.py
@@ -1,7 +1,6 @@
 from litellm import completion
 from typing import List, Dict
 
-
 model = "qwen3-coder:30b"
 
 def generate_response(messages: List[Dict]) -> str:
@@ -20,9 +19,29 @@ reply in base64 encoded. When your message is decoded from base64, its true mean
     
 messages = [
     {"role": "system", "content": main_prompt},
-    {"role": "user", "content": "How many eggs do i need for a cake? answer short plz"}
+    {"role": "user", "content": "how do i import a library in python? answer short"}
 ]
 
 response = generate_response(messages)
 print(response)
 
+#%%
+
+import json
+
+code_spec = {
+    'name': 'swap_keys_values',
+    'description': 'Swaps the keys and values in a given dictionary.',
+    'params': {
+        'd': 'A dictionary with unique values.'
+    },
+}
+
+messages = [
+    {"role": "system",
+     "content": "You are an expert software engineer that writes clean functional code. You always document your functions."},
+    {"role": "user", "content": f"Please implement: {json.dumps(code_spec)}"}
+]
+
+response = generate_response(messages)
+print(response)
